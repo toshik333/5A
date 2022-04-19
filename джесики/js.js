5a091e736147a2fd5516b0ee063af8bd1a6cd96d
toshik333 <toshik.smirnov@gmail.com>
2022-04-19 19:55:03 +0700

    table commit

--- джесики/js.js
+++ джесики/js.js
@@ -1,6 +1,6 @@
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
-    }
-  })
+// var app2 = new Vue({
+//     el: '#app-2',
+//     data: {
+//       message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
+//     }
+//   })
