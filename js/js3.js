ba85a7107f47fc420caf3119d19f63f625d04499
Your Name <you@example.com>
2022-05-10 16:55:54 +0700

    config all

---
+++ js/js3.js
@@ -0,0 +1,47 @@
+
+let char = {
+    hp: 10,
+    maxhp: 10,
+    mp:10,
+    maxmp: 10,
+    dmg:1,
+    name:"Лиса",
+    class:"noclass",
+    skills: {
+        punch: punch,
+        fireball: fair
+    },
+    avatar:"/picture/i.webp"
+}
+
+function update(char) {
+    $("#name").text(char.name);
+    $("#hp .line").css("width", `${char.hp * 100 / char.maxhp}%`)
+    $("#mp .line").css("width", `${char.mp * 100 / char.maxmp}%`)
+    $("#avatar").attr("src", char.avatar);
+}
+
+function punch(dmg) {
+    alert(`Вы нанесли ${dmg} урона!`)
+}
+function fair(dmg, mp) {
+    alert(`Вы нанесли ${dmg} урона!`)
+}
+
+update(char);
+for (skill in char.skills) {
+    let skill_elem = $("<button>").attr("id", skill).text(skill)
+    $("skills").append(skill_elem);
+    $(`#${skill}`).click(function () {
+        char.skills[skill](char.dmg, char.mp)
+    })
+    $(".skills").append(skill_elem)
+}
+
+
+setInterval(() => {
+    if(char.hp < char.maxhp) {
+        char.hp += 0.1
+    }
+    update(char)
+}, 450);
