82dd488fa3055989626dcdd5ff1eb25c144755fc
Крупникова Катя <91129555+krupnikova-kata@users.noreply.github.com>
2022-05-17 20:41:02 +0700

    config mov1

---
+++ js/js20.js
@@ -0,0 +1,50 @@
+
+let char = {
+    hp: 9,
+    maxhp: 10,
+    mp: 10,
+    maxmp: 10,
+    cp: 9,
+    maxcp: 10,
+    dmg: 1,
+    name: "MARUSTAR2010",
+    class: "noclass",
+    // skills: {
+    //     punch: punch,
+    //     fireball: fair
+    // },
+    avatar: "../picture/бигдан.jpg"
+}
+
+function update(char) {
+    $("#name").text(char.name);
+    $("#hp .line").css("width", `${char.hp * 100 / char.maxhp}%`)
+    $("#mp .line").css("width", `${char.mp * 100 / char.maxmp}%`)
+    $("#cp .line").css("width", `${char.cp * 100 / char.maxcp}%`)
+    $("#avatar").attr("src", char.avatar);
+}
+
+function punch(dmg) {
+    alert(`Вы нанесли ${dmg} урона!`)
+}
+function fair(dmg, mp) {
+    alert(`Вы нанесли ${dmg} урона!`)
+}
+
+update(char);
+for (skill in char.skills) {
+    let skill_elem = $("<button>").attr("id", skill).text(skill)
+    $("skills").append(skill_elem);
+    $(`#${skill}`).click(function () {
+        char.skills[skill](char.dmg, char.mp)
+    })
+    $(".skills").append(skill_elem)
+}
+
+
+// setInterval(() => {
+//     if(char.hp < char.maxhp) {
+//         char.hp += 0.1
+//     }
+//     update(char)
+// }, 450);
